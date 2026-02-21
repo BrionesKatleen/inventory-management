ab6924156c42699e8a19aa4ff3fd3ceca623af3a
Tamago! <brioneskatleen58@gmail.com>
2026-02-21 10:40:10 +0800

    Add initial Program.cs for Inventory Management

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace InventoryManagament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InventoryView view = new InventoryView();
+            
+            view.Run();
+        }
+    }
+}
